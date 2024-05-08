45c1030bdd81c702eecb3425eb2fec8c156a352e
Georg Dronov <dronovgeorg72@gmail.com>
2024-05-08 09:17:33 +0300

    feat: add counter

--- src/App.js
+++ src/App.js
@@ -1,20 +1,13 @@
 import React, { useState } from "react";
+import Counter from "./components/Counter";
 
 function App() {
-  const [likes, setlikes] = useState(0)
+  const [value, setValue] = useState("Input text")
 
-  function increment() {
-    setlikes(likes + 1)
-  }
-  function decrement() {
-    setlikes(likes - 1)
-  }
 
   return (
     <div className="App">
-      <h1>{likes}</h1>
-      <button onClick={increment}>Increment</button>
-      <button onClick={decrement}>Decrement</button>
+      <Counter />
     </div>
   );
 }
