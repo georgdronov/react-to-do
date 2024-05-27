36bf13a0c7c3bc83720e59182d40f9324274bf6f
Georg Dronov <dronovgeorg72@gmail.com>
2024-05-27 21:19:33 +0300

    add new components

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
 import React, { useState } from "react";
-import Counter from "./components/Counter";
-import ClassCounter from "./components/ClassCounter";
 import './styles/App.css'
-import { PostItem } from "./components/PostItem";
+import { PostList } from "./components/PostList";
+
 
 function App() {
-  const [value, setValue] = useState("Input text")
 
+  const [posts, setPosts] = useState([
+    { id: 1, title: "JavaScript", body: "Description" },
+    { id: 2, title: "JavaScript 2", body: "Description" },
+    { id: 3, title: "JavaScript 3", body: "Description" }
+  ])
 
   return (
     <div className="App">
-      <PostItem post={{ id: 1, title: "JavaScript", body: "Description" }} />
+      <PostList posts={posts} title={"Post list 1"}/>
 
     </div>
   );
