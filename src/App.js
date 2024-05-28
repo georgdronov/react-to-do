b0c6a836f942efa155f06fd13bfac8f57ac6d596
Georg Dronov <dronovgeorg72@gmail.com>
2024-05-28 17:15:38 +0300

    add feater create post

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { useState } from "react";
 import './styles/App.css'
 import { PostList } from "./components/PostList";
+import { MyButton } from "./components/UI/button/MyButton";
+import { MyInput } from "./components/UI/input/MyInput";
 
 
 function App() {
@@ -11,9 +13,34 @@ function App() {
     { id: 3, title: "JavaScript 3", body: "Description" }
   ])
 
+  const [post, setPost] = useState({ title: '', body: '' })
+  const addNewPost = (e) => {
+    e.preventDefault()
+    setPosts([...posts, {...post, id: Date.now()}])
+    setPost()
+  }
+
   return (
     <div className="App">
-      <PostList posts={posts} title={"Post list 1"}/>
+
+      <form>
+        <MyInput
+          value={post.title}
+          onChange={e => setPost({ ...post, title: e.target.value })}
+          type="text"
+          placeholder="Post name" />
+
+
+        <MyInput
+          value={post.body}
+          onChange={e => setPost({ ...post, body: e.target.value })}
+          type="text"
+          placeholder="Post description" />
+
+
+        <MyButton onClick={addNewPost}>Send post</MyButton>
+      </form>
+      <PostList posts={posts} title={"Post list 1"} />
 
     </div>
   );
