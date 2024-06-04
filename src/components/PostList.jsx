5ce784d179d3affe01a9dc44f4fc62752e24b07e
Georg Dronov <dronovgeorg72@gmail.com>
2024-06-04 19:11:11 +0300

    add animation

--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import { PostItem } from "./PostItem";
+import { TransitionGroup, CSSTransition } from "react-transition-group";
 
 
 export const PostList = ({ posts, title, remove }) => {
 
-    if(!posts.length){
+    if (!posts.length) {
         return (
-            <h1 style={{textAlign:'center'}}>
+            <h1 style={{ textAlign: 'center' }}>
                 Post list is empty
             </h1>
         )
@@ -15,9 +16,18 @@ export const PostList = ({ posts, title, remove }) => {
     return (
         <div>
             <h1 style={{ textAlign: 'center' }}>{title}</h1>
-            {posts.map((post, index) =>
-                <PostItem remove={remove} number={index + 1} post={post} key={post.id} />
-            )}
+            <TransitionGroup>
+                {posts.map((post, index) =>
+
+                    <CSSTransition
+                        key={post.id}
+                        timeout={500}
+                        classNames="post"
+                    >
+                        <PostItem remove={remove} number={index + 1} post={post} />
+                    </CSSTransition>
+                )}
+            </TransitionGroup>
         </div>
     );
 };
