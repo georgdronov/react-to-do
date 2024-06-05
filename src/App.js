0dd84a398319b5118991eeade8e96d3f1bdc4b58
Georg Dronov <dronovgeorg72@gmail.com>
2024-06-05 18:51:56 +0300

    add useEffect

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-import React, { useState } from "react";
-import './styles/App.css'
+import React, { useEffect, useState } from "react";
 import { PostList } from "./components/PostList";
 import { PostForm } from "./components/PostForm";
-
 import { PostFilter } from "./components/PostFilter";
 import { MyModal } from "./components/UI/MyModal/MyModal";
 import { MyButton } from "./components/UI/button/MyButton";
 import { usePosts } from "./hooks/usePost";
+import axios from "axios";
+import './styles/App.css'
 
 
 function App() {
@@ -16,13 +16,18 @@ function App() {
   const [modal, setModal] = useState(false)
   const sortedAndSearchPosts = usePosts(posts, filter.sort, filter.query)
 
-
-
-
+  useEffect(() => {
+    fetchPosts()
+  }, [])
 
   const createPost = (newPost) => {
     setPosts([...posts, newPost])
     setModal(false)
+  }
+
+  async function fetchPosts() {
+    const response = await axios.get('https://jsonplaceholder.typicode.com/posts')
+    setPosts(response.data)
   }
 
   const removePost = (post) => {
@@ -32,6 +37,7 @@ function App() {
 
   return (
     <div className="App">
+      <MyButton onClick={fetchPosts}>Get posts</MyButton>
       <MyButton
         style={{ marginTop: 30 }}
         onClick={() => setModal(true)}>
